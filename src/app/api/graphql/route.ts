1e4e34192bdd5911bf9c516dc9ce3a1650c94a31
RobertTheDev <robertthedev@gmail.com>
2024-02-28 20:38:44 +0000

    feat: set up apollo server

---
+++ src/app/api/graphql/route.ts
@@ -0,0 +1,14 @@
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+import { ApolloServer } from '@apollo/server';
+import { NextRequest } from 'next/server';
+import { schema } from './schema';
+
+const server = new ApolloServer({
+    schema
+});
+
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {
+    context: async req => ({ req })
+});
+
+export { handler as GET, handler as POST };
