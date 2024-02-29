726dc7d3afdf51d638c8fe53b894d7c6e4c522d9
RobertTheDev <robertthedev@gmail.com>
2024-02-29 00:21:57 +0000

    feat: defined and imported type defs

--- src/app/api/graphql/schema/typeDefs/index.ts
+++ src/app/api/graphql/schema/typeDefs/index.ts
@@ -1,6 +1,7 @@
 import { mergeTypeDefs } from '@graphql-tools/merge';
 import { fundraiserTypes } from './types/fundraiserTypes';
+import { authTypes } from './types/authTypes';
 
-const types = [fundraiserTypes];
+const types = [authTypes, fundraiserTypes];
 
 export const typeDefs = mergeTypeDefs(types);
