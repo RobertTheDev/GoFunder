65f128da295404cc8dd248f2d214de8418ef670c
RobertTheDev <robertthedev@gmail.com>
2024-02-29 03:13:37 +0000

    feat: defined and mapped footer links

--- src/app/components/layout/Footer/index.tsx
+++ src/app/components/layout/Footer/index.tsx
@@ -1,7 +1,31 @@
+import Link from 'next/link';
+
 export default function Footer() {
+    const footerLinks: {
+        href: string;
+        name: string;
+    }[] = [
+        {
+            href: '/about',
+            name: 'About'
+        },
+        { href: '/accessibility-statement', name: 'Accessibility Statement' },
+        { href: '/privacy-policy', name: 'Privacy Policy' },
+        { href: '/terms-and-conditions', name: 'Terms and Conditions' }
+    ];
+
     return (
         <footer>
-            <p>Footer</p>
+            <div>
+                <p>©{new Date().getFullYear()} GoFunder</p>
+                <nav>
+                    {footerLinks.map(link => (
+                        <Link href={link.href} key={link.href}>
+                            {link.name}
+                        </Link>
+                    ))}
+                </nav>
+            </div>
         </footer>
     );
 }
