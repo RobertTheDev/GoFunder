b69dcd455163b2a88b16fc96f4847e56461c1e95
RobertTheDev <robertthedev@gmail.com>
2024-02-29 03:21:18 +0000

    feat: defined and mapped profile menu links

--- src/app/components/profile/ProfileMenu/index.tsx
+++ src/app/components/profile/ProfileMenu/index.tsx
@@ -1,7 +1,39 @@
+import Link from 'next/link';
+
 export default function ProfileMenu() {
+    const profileMenuLinks: {
+        href: string;
+        name: string;
+    }[] = [
+        {
+            href: '/profile',
+            name: 'Profile'
+        },
+        {
+            href: '/account-settings',
+            name: 'Account Settings'
+        },
+        {
+            href: '/my-donations',
+            name: 'My Donations'
+        },
+        {
+            href: '/my-fundraisers',
+            name: 'My Fundraisers'
+        }
+    ];
+
     return (
         <div>
             <p>Profile Menu</p>
+            <nav>
+                {profileMenuLinks.map(link => (
+                    <Link key={link.name} href={link.href}>
+                        {link.name}
+                    </Link>
+                ))}
+            </nav>
+            <button type="button">Sign Out</button>
         </div>
     );
 }
