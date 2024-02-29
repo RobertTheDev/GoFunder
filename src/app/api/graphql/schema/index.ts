b5dc4145945b03231be0f300d3fdab93ca99dfd1
RobertTheDev <robertthedev@gmail.com>
2024-02-29 00:22:26 +0000

    feat: defined schema

--- src/app/api/graphql/schema/index.ts
+++ src/app/api/graphql/schema/index.ts
@@ -1,10 +1,11 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { typeDefs } from './typeDefs';
 import { fundraiserResolvers } from './resolvers/fundraiserResolvers';
+import { authResolvers } from './resolvers/authResolvers';
 
 export const schema = makeExecutableSchema({
     typeDefs,
-    resolvers: [fundraiserResolvers]
+    resolvers: [authResolvers, fundraiserResolvers]
     // logger, // optional
     // resolverValidationOptions: {}, // optional
     // parseOptions: {}, // optional
