099ab7c3d86be6a99c535633a1b5a171afb71a71
RobertTheDev <robertthedev@gmail.com>
2024-02-29 17:48:12 +0000

    feat: added pepper

--- src/app/api/utils/password/passwordManagement.ts
+++ src/app/api/utils/password/passwordManagement.ts
@@ -1,14 +1,19 @@
 import { hash, verify } from 'argon2';
 
+// A distinct 32 character string added to password before hashing.
+const pepper = process.env.PEPPER;
+
+// This handler hashed a password with a unique password.
 export function hashPassword(password: string): Promise<string> {
-    return hash(password);
+    return hash(password + pepper);
 }
 
+// This handler checks id the inputted password matches the hashed password from the database.
 export function verifyPassword(params: {
     hashedPassword: string;
     inputtedPassword: string;
 }): Promise<boolean> {
     const { hashedPassword, inputtedPassword } = params;
 
-    return verify(inputtedPassword, hashedPassword);
+    return verify(inputtedPassword + pepper, hashedPassword);
 }
