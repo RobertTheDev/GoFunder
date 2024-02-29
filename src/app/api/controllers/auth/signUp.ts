e5e372e71082f5510aca326d4737186e7461e542
RobertTheDev <robertthedev@gmail.com>
2024-02-29 15:43:12 +0000

    refactor: updated import paths

--- src/app/api/controllers/auth/signUp.ts
+++ src/app/api/controllers/auth/signUp.ts
@@ -1,7 +1,7 @@
 import { GraphQLError } from 'graphql';
 import { User } from '@prisma/client';
-import { signUpSchema } from '../../../../../validations/auth/signUp.schema';
-import { prismaClient } from '../../db/prisma/prismaClient';
+import { signUpSchema } from '@/app/lib/validations/auth/signUp.schema';
+import { prismaClient } from '@/app/api/db/prisma/prismaClient';
 
 export default async function signUpController(
     input: unknown
