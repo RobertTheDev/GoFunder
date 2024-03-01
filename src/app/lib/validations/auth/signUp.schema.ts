9d95989d1e43ee1bb308adcd80ef82ab5e02f498
RobertTheDev <robertthedev@gmail.com>
2024-03-01 16:33:50 +0000

    refactor: removed password field

--- src/app/lib/validations/auth/signUp.schema.ts
+++ src/app/lib/validations/auth/signUp.schema.ts
@@ -8,15 +8,6 @@ export const signUpSchema = object({
     })
         .email({ message: 'Email must be in valid email format' })
         .min(1, { message: 'Email is required' }),
-    password: z
-        .string({
-            invalid_type_error: 'Password must be of type string',
-            required_error: 'Password must be at least 8 characters long'
-        })
-        .min(8, { message: 'Password must be at least 8 characters long' })
-        .max(20, {
-            message: 'Password must be no longer than 20 characters long'
-        }),
     name: string({
         invalid_type_error: 'Name must be of type string',
         required_error: 'Name is required'
