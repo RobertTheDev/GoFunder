a1651d41d3db17ad2922b6d12a3198b0d0637370
RobertTheDev <robertthedev@gmail.com>
2024-03-01 01:55:54 +0000

    feat: added layout styles

--- src/app/components/layout/PageLayout/index.tsx
+++ src/app/components/layout/PageLayout/index.tsx
@@ -1,13 +1,16 @@
+'use client';
+
 import { ReactNode } from 'react';
 import Footer from '../Footer';
 import Header from '../Header';
+import { StyledPageContainer } from './styles';
 
 export default function PageLayout({ children }: { children: ReactNode }) {
     return (
-        <div>
+        <StyledPageContainer>
             <Header />
             <main>{children}</main>
             <Footer />
-        </div>
+        </StyledPageContainer>
     );
 }
