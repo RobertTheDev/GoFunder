db534bd18129a81fb0295eca69b0efb822b997f8
RobertTheDev <robertthedev@gmail.com>
2024-03-01 21:59:16 +0000

    feat: created and imported change email schema and form

---
+++ src/app/components/account-settings/ChangeEmailForm/changeEmail.schema.ts
@@ -0,0 +1,18 @@
+import { z, object, string } from 'zod';
+
+// Schema defines the fields required to change an email.
+export const changeEmailSchema = object({
+    email: string({
+        invalid_type_error: 'Email must be of type string',
+        required_error: 'Email is required'
+    })
+        .email({ message: 'Email must be in valid email format' })
+        .min(1, { message: 'Email is required' }),
+    password: string({
+        invalid_type_error: 'Password must be of type string',
+        required_error: 'Password is required'
+    }).min(1, { message: 'Password is required' })
+});
+
+// Infers the schema as a TypeScipt type.
+export type ChangeEmailSchemaType = z.infer<typeof changeEmailSchema>;
--- src/app/components/account-settings/ChangeEmailForm/index.tsx
+++ src/app/components/account-settings/ChangeEmailForm/index.tsx
@@ -1,7 +1,43 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { ChangeEmailSchemaType, changeEmailSchema } from './changeEmail.schema';
+
 export default function ChangeEmailForm() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<ChangeEmailSchemaType>({
+        resolver: zodResolver(changeEmailSchema)
+    });
+
+    function handleChangeEmail(values: ChangeEmailSchemaType) {
+        // eslint-disable-next-line no-alert
+        alert(JSON.stringify(values));
+    }
+
     return (
-        <div>
-            <p>Change Email Form</p>
-        </div>
+        <form
+            onSubmit={handleSubmit(values => {
+                // eslint-disable-next-line no-alert
+                handleChangeEmail(values);
+            })}
+        >
+            <label htmlFor="email">
+                Email:
+                <input type="email" {...register('email')} />
+                {errors?.email && <p>{errors.email.message}</p>}
+            </label>
+            <br />
+            <label htmlFor="password">
+                Email:
+                <input type="password" {...register('password')} />
+                {errors?.password && <p>{errors.password.message}</p>}
+            </label>
+            <br />
+            <button type="submit">Change Email</button>
+        </form>
     );
 }
