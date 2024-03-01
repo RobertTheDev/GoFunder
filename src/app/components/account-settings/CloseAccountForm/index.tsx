904efc4903dfd6d5773714f4cc28382f6050e902
RobertTheDev <robertthedev@gmail.com>
2024-03-01 23:52:07 +0000

    feat: created and imported close account schema and created form

--- src/app/components/account-settings/CloseAccountForm/index.tsx
+++ src/app/components/account-settings/CloseAccountForm/index.tsx
@@ -1,7 +1,46 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+    closeAccountSchema,
+    CloseAccountSchemaType
+} from './closeAccount.schema';
+
 export default function CloseAccountForm() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<CloseAccountSchemaType>({
+        resolver: zodResolver(closeAccountSchema)
+    });
+
+    function handleCloseAcount(values: CloseAccountSchemaType) {
+        // eslint-disable-next-line no-alert
+        alert(JSON.stringify(values));
+    }
+
     return (
-        <div>
-            <p>Close Account Form</p>
-        </div>
+        <form
+            onSubmit={handleSubmit(values => {
+                // eslint-disable-next-line no-alert
+                handleCloseAcount(values);
+            })}
+        >
+            <label htmlFor="confirm">
+                Confirm:
+                <input type="text" {...register('confirm')} />
+                {errors?.confirm && <p>{errors.confirm.message}</p>}
+            </label>
+            <br />
+            <label htmlFor="password">
+                Email:
+                <input type="password" {...register('password')} />
+                {errors?.password && <p>{errors.password.message}</p>}
+            </label>
+            <br />
+            <button type="submit">Close Account</button>
+        </form>
     );
 }
