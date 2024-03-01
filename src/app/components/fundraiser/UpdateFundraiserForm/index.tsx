a9d962c70eeeae276f3ae98788c937b197ce1748
RobertTheDev <robertthedev@gmail.com>
2024-03-01 23:57:05 +0000

    feat: created and imported update fundraiser schema and created form

--- src/app/components/fundraiser/UpdateFundraiserForm/index.tsx
+++ src/app/components/fundraiser/UpdateFundraiserForm/index.tsx
@@ -1,7 +1,40 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+    updateFundraiserSchema,
+    UpdateFundraiserSchemaType
+} from './updateFundraiser.schema';
+
 export default function UpdateFundraiserForm() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<UpdateFundraiserSchemaType>({
+        resolver: zodResolver(updateFundraiserSchema)
+    });
+
+    function handleUpdateFundraiser(values: UpdateFundraiserSchemaType) {
+        // eslint-disable-next-line no-alert
+        alert(JSON.stringify(values));
+    }
+
     return (
-        <div>
-            <p>Update Fundraiser</p>
-        </div>
+        <form
+            onSubmit={handleSubmit(values => {
+                // eslint-disable-next-line no-alert
+                handleUpdateFundraiser(values);
+            })}
+        >
+            <label htmlFor="name">
+                Confirm:
+                <input type="text" {...register('name')} />
+                {errors?.name && <p>{errors.name.message}</p>}
+            </label>
+            <br />
+            <button type="submit">Update Fundraiser</button>
+        </form>
     );
 }
