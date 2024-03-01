5e1811875fdfc16a5fe858ae1ee38d3019a07ca7
RobertTheDev <robertthedev@gmail.com>
2024-03-01 23:56:25 +0000

    feat: created and mapped profile menu links

---
+++ src/app/components/profile/ProfileMenu/profileMenuLinks.ts
@@ -0,0 +1,23 @@
+const profileMenuLinks: {
+    name: string;
+    path: string;
+}[] = [
+    {
+        name: 'Profile',
+        path: '/profile'
+    },
+    {
+        name: 'Account Settings',
+        path: '/account-settings'
+    },
+    {
+        name: 'My Donations',
+        path: '/my-donations'
+    },
+    {
+        name: 'My Fundraisers',
+        path: '/my-fundraisers'
+    }
+];
+
+export default profileMenuLinks;
