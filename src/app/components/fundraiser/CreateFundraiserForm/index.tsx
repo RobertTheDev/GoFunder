445125bdbbb618b9c2dc7d280ea839f39cbccca3
RobertTheDev <robertthedev@gmail.com>
2024-03-01 23:52:43 +0000

    feat: created and imported create fundraiser schema and created form

--- src/app/components/fundraiser/CreateFundraiserForm/index.tsx
+++ src/app/components/fundraiser/CreateFundraiserForm/index.tsx
@@ -1,7 +1,40 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+    CreateFundraiserSchemaType,
+    createFundraiserSchema
+} from './createFundraiser.schema';
+
 export default function CreateFundraiserForm() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<CreateFundraiserSchemaType>({
+        resolver: zodResolver(createFundraiserSchema)
+    });
+
+    function handleCreateFundraiser(values: CreateFundraiserSchemaType) {
+        // eslint-disable-next-line no-alert
+        alert(JSON.stringify(values));
+    }
+
     return (
-        <div>
-            <p>Create Fundraiser</p>
-        </div>
+        <form
+            onSubmit={handleSubmit(values => {
+                // eslint-disable-next-line no-alert
+                handleCreateFundraiser(values);
+            })}
+        >
+            <label htmlFor="name">
+                Confirm:
+                <input type="text" {...register('name')} />
+                {errors?.name && <p>{errors.name.message}</p>}
+            </label>
+            <br />
+            <button type="submit">Create Fundraiser</button>
+        </form>
     );
 }
