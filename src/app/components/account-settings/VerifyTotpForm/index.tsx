009703356fe84d3bf9604bdbc72ac2176cbbfb9b
RobertTheDev <robertthedev@gmail.com>
2024-03-02 00:12:36 +0000

    feat: created and imported verify totp form schema

--- src/app/components/account-settings/VerifyTotpForm/index.tsx
+++ src/app/components/account-settings/VerifyTotpForm/index.tsx
@@ -1,7 +1,40 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+    verifyTotpCodeSchema,
+    type VerifyTotpCodeSchemaType
+} from './verifyTotpCode.schema';
+
 export default function VerifyTotpForm() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<VerifyTotpCodeSchemaType>({
+        resolver: zodResolver(verifyTotpCodeSchema)
+    });
+
+    function handleVerifyTotp(values: VerifyTotpCodeSchemaType) {
+        // eslint-disable-next-line no-alert
+        alert(JSON.stringify(values));
+    }
+
     return (
-        <div>
-            <p>Verify Totp Form</p>
-        </div>
+        <form
+            onSubmit={handleSubmit(values => {
+                // eslint-disable-next-line no-alert
+                handleVerifyTotp(values);
+            })}
+        >
+            <label htmlFor="code">
+                Code:
+                <input type="text" {...register('code')} />
+                {errors?.code && <p>{errors.code.message}</p>}
+            </label>
+            <br />
+            <button type="submit">Verify TOTP</button>
+        </form>
     );
 }
