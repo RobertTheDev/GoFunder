5b042fad869484845e4c635572e0d9fbf556bba4
RobertTheDev <robertthedev@gmail.com>
2024-03-02 19:50:33 +0000

    feat: created use profile menu hook

---
+++ src/app/hooks/profileMenu/useProfileMenu.tsx
@@ -0,0 +1,29 @@
+import { MutableRefObject, useRef, useState } from 'react';
+import { useOnClickOutside } from 'usehooks-ts';
+
+export function useProfileMenu(): {
+    profileMenuActive: boolean;
+    profileMenuRef: MutableRefObject<HTMLDivElement | null>;
+    toggleProfileMenu: () => void;
+} {
+    const [profileMenuActive, setProfileMenuActive] = useState<boolean>(false);
+
+    const profileMenuRef: MutableRefObject<HTMLDivElement | null> =
+        useRef<HTMLDivElement | null>(null);
+
+    function toggleProfileMenu(): void {
+        setProfileMenuActive(!profileMenuActive);
+    }
+
+    function closeProfileMenu(): void {
+        setProfileMenuActive(false);
+    }
+
+    useOnClickOutside(profileMenuRef, closeProfileMenu);
+
+    return {
+        profileMenuActive,
+        profileMenuRef,
+        toggleProfileMenu
+    };
+}
