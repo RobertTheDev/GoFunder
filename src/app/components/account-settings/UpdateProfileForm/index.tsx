d7cad8d550326ade3271fc893f204f2c8ed0e846
RobertTheDev <robertthedev@gmail.com>
2024-03-02 00:13:07 +0000

    feat: created and imported update profile form schema

--- src/app/components/account-settings/UpdateProfileForm/index.tsx
+++ src/app/components/account-settings/UpdateProfileForm/index.tsx
@@ -1,7 +1,46 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+    updateProfileSchema,
+    UpdateProfileSchemaType
+} from './updateProfile.schema';
+
 export default function UpdateProfileForm() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<UpdateProfileSchemaType>({
+        resolver: zodResolver(updateProfileSchema)
+    });
+
+    function handleCloseAcount(values: UpdateProfileSchemaType) {
+        // eslint-disable-next-line no-alert
+        alert(JSON.stringify(values));
+    }
+
     return (
-        <div>
-            <p>Update Profile Form</p>
-        </div>
+        <form
+            onSubmit={handleSubmit(values => {
+                // eslint-disable-next-line no-alert
+                handleCloseAcount(values);
+            })}
+        >
+            <label htmlFor="name">
+                Name:
+                <input type="text" {...register('name')} />
+                {errors?.name && <p>{errors.name.message}</p>}
+            </label>
+            <br />
+            <label htmlFor="annonymous">
+                Annonymous:
+                <input type="checkbox" {...register('annonymous')} />
+                {errors?.annonymous && <p>{errors.annonymous.message}</p>}
+            </label>
+            <br />
+            <button type="submit">Update Profile</button>
+        </form>
     );
 }
