035c18632e9d73c77667d77fabe149910b7883a9
RobertTheDev <robertthedev@gmail.com>
2024-03-02 00:13:31 +0000

    feat: created and imported set up totp form schema

--- src/app/components/account-settings/SetUpTotpForm/index.tsx
+++ src/app/components/account-settings/SetUpTotpForm/index.tsx
@@ -1,7 +1,41 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+    setUpTotpCodeSchema,
+    SetUpTotpCodeSchemaType
+} from './setUpTotp.schema';
+
 export default function SetUpTotpForm() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<SetUpTotpCodeSchemaType>({
+        resolver: zodResolver(setUpTotpCodeSchema)
+    });
+
+    function handleCloseAcount(values: SetUpTotpCodeSchemaType) {
+        // eslint-disable-next-line no-alert
+        alert(JSON.stringify(values));
+    }
+
     return (
-        <div>
-            <p>Set Up Totp Form</p>
-        </div>
+        <form
+            onSubmit={handleSubmit(values => {
+                // eslint-disable-next-line no-alert
+                handleCloseAcount(values);
+            })}
+        >
+            <label htmlFor="mfaSecret">
+                MFA Secret:
+                <input type="text" {...register('mfaSecret')} />
+                {errors?.mfaSecret && <p>{errors.mfaSecret.message}</p>}
+            </label>
+            <br />
+
+            <button type="submit">Set Up TOTP</button>
+        </form>
     );
 }
