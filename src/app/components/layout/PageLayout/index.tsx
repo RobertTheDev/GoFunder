41127b381986fe43b3a80bff4fa81f82568d1e08
RobertTheDev <robertthedev@gmail.com>
2024-03-02 17:48:18 +0000

    feat: installed and set up n-progress

--- src/app/components/layout/PageLayout/index.tsx
+++ src/app/components/layout/PageLayout/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ReactNode } from 'react';
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 import Footer from '../Footer';
 import Header from '../Header';
 import { StyledPageContainer } from './styles';
@@ -8,6 +9,13 @@ import { StyledPageContainer } from './styles';
 export default function PageLayout({ children }: { children: ReactNode }) {
     return (
         <StyledPageContainer>
+            <ProgressBar
+                height="4px"
+                color="#fffd00"
+                options={{ showSpinner: false }}
+                shallowRouting
+                delay={250}
+            />
             <Header />
             <main>{children}</main>
             <Footer />
