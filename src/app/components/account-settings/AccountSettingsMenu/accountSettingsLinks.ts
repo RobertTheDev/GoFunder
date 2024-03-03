cae533e7e232b0d7debdf7588a418b7f64617f63
RobertTheDev <robertthedev@gmail.com>
2024-03-03 06:10:08 +0000

    feat: defined account settings links

--- src/app/components/account-settings/AccountSettingsMenu/accountSettingsLinks.ts
+++ src/app/components/account-settings/AccountSettingsMenu/accountSettingsLinks.ts
@@ -1,3 +1,12 @@
-const accountSettingsLinks = [{ name: '', path: '' }];
+const accountSettingsLinks: {
+    name: string;
+    path: string;
+}[] = [
+    { name: 'Update Profile', path: '/update-profile' },
+    { name: 'Change Email', path: '/change-email' },
+    { name: 'Change Password', path: '/change-password' },
+    { name: 'Close Account', path: '/close-account' },
+    { name: 'Set Up TOTP', path: '/set-up-totp' }
+];
 
 export default accountSettingsLinks;
