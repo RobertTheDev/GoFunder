1fe6e70095ef43531dccad39e800023c6523e1b3
RobertTheDev <robertthedev@gmail.com>
2024-03-03 06:09:49 +0000

    feat: set up sass options

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    compiler: { styledComponents: true },
+    sassOptions: {
+        includePaths: [path.join(dirname, 'styles')]
+    },
     images: {
         remotePatterns: [
             {
