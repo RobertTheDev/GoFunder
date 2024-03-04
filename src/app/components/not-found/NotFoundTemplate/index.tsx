06f6198a719c91d2ac1719cb4a4be4956996621e
RobertTheDev <robertthedev@gmail.com>
2024-03-04 16:38:19 +0000

    feat: added not found content

--- src/app/components/not-found/NotFoundTemplate/index.tsx
+++ src/app/components/not-found/NotFoundTemplate/index.tsx
@@ -1,7 +1,12 @@
+import Image from 'next/image';
+
 export default function NotFoundTemplate() {
     return (
-        <div>
-            <h1>Not Found</h1>
+        <div className="not-found-page-container">
+            <h1 className="not-found-page-title">404 - Not Found</h1>
+            <div className="not-found-page-image-container">
+                <Image src="/404.svg" alt="404" fill />
+            </div>
         </div>
     );
 }
