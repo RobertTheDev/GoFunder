5f1a0a378a0041312b2eb206f801779feafab739
RobertTheDev <robertthedev@gmail.com>
2024-03-05 03:56:07 +0000

    feat: defined auth types

--- src/app/api/graphql/schema/typeDefs/types/authTypes.ts
+++ src/app/api/graphql/schema/typeDefs/types/authTypes.ts
@@ -1,13 +1,6 @@
 import gql from 'graphql-tag';
 
 export const authTypes = gql`
-    type User {
-        id: String
-        email: String
-        password: String
-        name: String
-    }
-
     type Mutation {
         signUp(email: String, name: String, password: String): User
     }
