4183c2395d15edf2806bf12f0e5dd9c6b2ca4a50
RobertTheDev <robertthedev@gmail.com>
2024-03-05 07:55:03 +0000

    feat: created and added home page template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,6 @@
-import { getClient } from '@/app/lib/apollo/apolloClient';
-import { gql } from '@apollo/client';
-import { Fundraiser } from '@prisma/client';
-import FundraiserCard from './components/fundraiser/FundraiserCard';
+import { JSX } from 'react';
+import HomeTemplate from '@/app/templates/home/HomeTemplate';
 
-const GET_FUNDRAISERS = gql`
-    query getFundraisers {
-        fundraisers {
-            id
-            image
-            name
-            slug
-            target
-            totalDonations
-            totalRaised
-        }
-    }
-`;
-
-export default async function HomePage() {
-    const client = getClient();
-
-    const {
-        loading,
-        error,
-        data: { fundraisers }
-    } = await client.query({
-        query: GET_FUNDRAISERS
-    });
-
-    if (error) return <p>There was an error</p>;
-    if (loading) return <p>Loading...</p>;
-
-    return (
-        <div className="fundraisers-page-container">
-            <div className="fundraiser-card-grid">
-                {fundraisers.map((fundraiser: Fundraiser) => (
-                    <FundraiserCard {...fundraiser} key={fundraiser.id} />
-                ))}
-            </div>
-        </div>
-    );
+export default function HomePage(): JSX.Element {
+    return <HomeTemplate />;
 }
---
+++ src/app/templates/home/HomeTemplate/index.tsx
@@ -0,0 +1,9 @@
+import { JSX } from 'react';
+
+export default function HomeTemplate(): JSX.Element {
+    return (
+        <div>
+            <p>Home</p>
+        </div>
+    );
+}
