c0c9369bd957a4ce3502ef854608c07401765143
RobertTheDev <robertthedev@gmail.com>
2024-03-05 03:58:19 +0000

    feat: defined and imported date resolvers

--- src/app/api/graphql/schema/index.ts
+++ src/app/api/graphql/schema/index.ts
@@ -2,10 +2,11 @@ import { makeExecutableSchema } from '@graphql-tools/schema';
 import { typeDefs } from './typeDefs';
 import { fundraiserResolvers } from './resolvers/fundraiserResolvers';
 import { authResolvers } from './resolvers/authResolvers';
+import { dateResolvers } from './resolvers/dateResolvers';
 
 export const schema = makeExecutableSchema({
     typeDefs,
-    resolvers: [authResolvers, fundraiserResolvers]
+    resolvers: [dateResolvers, authResolvers, fundraiserResolvers]
     // logger, // optional
     // resolverValidationOptions: {}, // optional
     // parseOptions: {}, // optional
