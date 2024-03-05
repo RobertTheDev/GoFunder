1ad3cddb270a20c1b85c616ebe2dc0d8ee244e7a
RobertTheDev <robertthedev@gmail.com>
2024-03-05 01:50:06 +0000

    feat: added new fields to fundraiser type

--- src/app/api/graphql/schema/typeDefs/types/fundraiserTypes.ts
+++ src/app/api/graphql/schema/typeDefs/types/fundraiserTypes.ts
@@ -1,13 +1,34 @@
 import gql from 'graphql-tag';
 
 export const fundraiserTypes = gql`
+    type User {
+        id: String
+        name: String
+        email: String
+        username: String
+        image: String
+    }
+
+    type Donation {
+        id: String
+        amount: Int
+        message: String
+        user: User
+    }
+
     type Fundraiser {
         id: String!
         image: String!
         name: String!
+        slug: String!
+        target: Int!
+        totalDonations: Int!
+        totalRaised: Int!
+        donations: [Donation]
     }
 
     type Query {
+        fundraiserBySlug(slug: String): Fundraiser
         fundraisers: [Fundraiser]
     }
 `;
