4d39c2187777fb82c9f45adb5b7332fd3d1521f2
RobertTheDev <robertthedev@gmail.com>
2024-03-05 03:56:35 +0000

    feat: added schema types

--- src/app/api/graphql/schema/typeDefs/index.ts
+++ src/app/api/graphql/schema/typeDefs/index.ts
@@ -1,7 +1,18 @@
 import { mergeTypeDefs } from '@graphql-tools/merge';
 import { fundraiserTypes } from './types/fundraiserTypes';
 import { authTypes } from './types/authTypes';
+import { donationTypes } from './types/donationTypes';
+import { savedFundraiserTypes } from './types/savedFundraiserTypes';
+import { userTypes } from './types/userTypes';
+import { dateTypes } from './types/dateTypes';
 
-const types = [authTypes, fundraiserTypes];
+const types = [
+    authTypes,
+    dateTypes,
+    donationTypes,
+    fundraiserTypes,
+    savedFundraiserTypes,
+    userTypes
+];
 
 export const typeDefs = mergeTypeDefs(types);
