8977f69d7556949b67fb253db3d32b2ec4fe1c41
RobertTheDev <robertthedev@gmail.com>
2024-03-05 06:59:03 +0000

    feat: added fundraiser props

--- src/app/templates/fundraiser/FundraiserTemplate/index.tsx
+++ src/app/templates/fundraiser/FundraiserTemplate/index.tsx
@@ -1,21 +1,59 @@
 'use client';
 
-import { Fundraiser } from '@prisma/client';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { format } from 'date-fns';
+import DonationCard from '@/app/components/donation/DonationCard';
+import { IFundraiser } from '@/app/interfaces/Fundraiser';
 
-export default function FundraiserTemplate(params: { fundraiser: Fundraiser }) {
+export default function FundraiserTemplate(params: {
+    fundraiser: IFundraiser;
+}) {
     const { fundraiser } = params;
+
+    const router = useRouter();
+
+    const {
+        name,
+        category,
+        description,
+        deadlineDate,
+        createdAt,
+        image,
+        slug,
+        target,
+        totalDonations,
+        totalRaised,
+        donations
+    } = fundraiser;
 
     return (
         <div>
-            <p>{fundraiser.name}</p>
-            <p>{fundraiser.image}</p>
-            <p>{JSON.stringify(fundraiser)}</p>
-            {/* <button
+            <Image src={image} alt={name} height={400} width={400} />
+
+            <p>{name}</p>
+            <p>{category}</p>
+            <p>{format(new Date(createdAt), 'yyyy')}</p>
+            {deadlineDate && <p>{format(new Date(deadlineDate), 'yyyy')}</p>}
+
+            <p>{target}</p>
+            <p> {totalDonations}</p>
+            <p>{totalRaised}</p>
+
+            <p>{description}</p>
+            <button
                 type="button"
-                onClick={() => router.push(`/fundraisers/${route.slug}/donate`)}
+                onClick={() => router.push(`/fundraisers/${slug}/donate`)}
             >
                 Donate
-            </button> */}
+            </button>
+            <button type="button" onClick={() => {}}>
+                Save Fundraiser
+            </button>
+
+            {donations.map(donation => (
+                <DonationCard key={donation.id} donation={donation} />
+            ))}
         </div>
     );
 }
