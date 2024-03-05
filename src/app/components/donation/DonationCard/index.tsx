2778c3e29819a29577b441e70ffb014856f89b3b
RobertTheDev <robertthedev@gmail.com>
2024-03-05 07:02:59 +0000

    feat: added donation interface

--- src/app/components/donation/DonationCard/index.tsx
+++ src/app/components/donation/DonationCard/index.tsx
@@ -1,14 +1,29 @@
 'use client';
 
-import { Donation } from '@prisma/client';
+import { IDonation } from '@/app/interfaces/Donation';
+import Image from 'next/image';
 
-export default function DonationCard(donation: Donation) {
-    const { amount, message } = donation;
+export default function DonationCard(params: { donation: IDonation }) {
+    const {
+        donation: { amount, message, user }
+    } = params;
 
     return (
         <div>
             <p>{message}</p>
-            <p>{amount}</p>
+            <p>£{amount.toLocaleString()}</p>
+
+            {user.image ? (
+                <Image src={user.image} alt="dd" height={120} width={120} />
+            ) : (
+                <Image
+                    src="/defaultAvatar.png"
+                    alt="dd"
+                    height={120}
+                    width={120}
+                />
+            )}
+            <p>{user.username}</p>
         </div>
     );
 }
