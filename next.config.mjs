57420699271d420ad637a6431d697c82ba236c90
RobertTheDev <robertthedev@gmail.com>
2024-03-07 15:38:25 +0000

    ci: removed sass config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const dirname = path.dirname(fileURLToPath(import.meta.url));
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    sassOptions: {
-        includePaths: [path.join(dirname, 'styles')]
-    },
     images: {
         remotePatterns: [
             {
