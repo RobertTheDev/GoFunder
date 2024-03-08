672d0a9fac46158818db8783c347abae3e786a27
RobertTheDev <robertthedev@gmail.com>
2024-03-08 16:17:34 +0000

    feat: created create donation template

---
+++ src/app/modules/donation/templates/CreateDonationTemplate/index.tsx
@@ -0,0 +1,10 @@
+import { JSX } from 'react';
+import CreateDonationForm from '../../components/CreateDonationForm';
+
+export default function CreateDonationTemplate(): JSX.Element {
+    return (
+        <div>
+            <CreateDonationForm />
+        </div>
+    );
+}
