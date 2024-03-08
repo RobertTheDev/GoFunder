7d06aa31c34a0a2a75f184b4fa729fc49f5ee190
RobertTheDev <robertthedev@gmail.com>
2024-03-08 15:06:33 +0000

    feat: moved home template and components into home module and updated import paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { JSX } from 'react';
-import HomeTemplate from '@/app/templates/home/HomeTemplate';
+import HomeTemplate from '@/app/modules/home/template/HomeTemplate';
 
 export default function HomePage(): JSX.Element {
     return <HomeTemplate />;
