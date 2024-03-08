5a917a2f19a2a0a022029ae1f43bd63612012365
RobertTheDev <robertthedev@gmail.com>
2024-03-08 15:57:32 +0000

    refactor: moved auth layout to auth module and updated import paths

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -3,7 +3,7 @@
 // The relevant imports required for the page.
 import { JSX } from 'react';
 import SignUpForm from '@/app/modules/auth/components/SignUpForm';
-import AuthPageTemplate from '@/app/templates/auth/AuthPage';
+import AuthPageTemplate from '@/app/modules/auth/layouts/AuthLayout';
 import { Metadata } from 'next';
 
 // Metadata defines the seo options for this page.
