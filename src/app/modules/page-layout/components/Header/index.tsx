910dee6a4160e3df26dc74f31be0a4a3ea543403
RobertTheDev <robertthedev@gmail.com>
2024-03-09 09:41:18 +0000

    feat: added profile avatar

--- src/app/modules/page-layout/components/Header/index.tsx
+++ src/app/modules/page-layout/components/Header/index.tsx
@@ -55,7 +55,7 @@ export default function Header() {
 
                 <input
                     type="image"
-                    src="/defaultAvatar.png"
+                    src="https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?q=80&w=2952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                     alt="avatar image"
                     className={styles.headerAvatarInput}
                     onClick={() => toggleProfileMenu()}
