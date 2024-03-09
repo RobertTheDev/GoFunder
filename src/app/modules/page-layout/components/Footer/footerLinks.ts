07359610b9a89540ab3c9d5c15fdf7fc359d6bdf
RobertTheDev <robertthedev@gmail.com>
2024-03-09 14:47:24 +0000

    refactor: removed export default

--- src/app/modules/page-layout/components/Footer/footerLinks.ts
+++ src/app/modules/page-layout/components/Footer/footerLinks.ts
@@ -1,10 +1,5 @@
-const footerLinks: {
-    href: string;
-    name: string;
-}[] = [
+export const footerLinks = [
     { href: '/info/accessibility-statement', name: 'Accessibility Statement' },
     { href: '/info/privacy-policy', name: 'Privacy Policy' },
     { href: '/info/terms-and-conditions', name: 'Terms and Conditions' }
 ];
-
-export default footerLinks;
--- src/app/modules/page-layout/components/Header/headerLinks.ts
+++ src/app/modules/page-layout/components/Header/headerLinks.ts
@@ -1,10 +1,5 @@
-const headerLinks: {
-    href: string;
-    name: string;
-}[] = [
+export const headerLinks = [
     { href: '/', name: 'Home' },
     { href: '/fundraisers', name: 'Explore' },
     { href: '/create-fundraiser', name: 'Start Fundraising' }
 ];
-
-export default headerLinks;
