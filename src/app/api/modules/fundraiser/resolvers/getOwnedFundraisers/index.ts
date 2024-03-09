f4e585f8da30f26f30a1515f029913d9f4c23bd7
RobertTheDev <robertthedev@gmail.com>
2024-03-09 09:36:38 +0000

    feat: defined resolver

---
+++ src/app/api/modules/fundraiser/resolvers/getOwnedFundraisers/index.ts
@@ -0,0 +1,7 @@
+import { prismaClient } from '@/app/api/db/prisma/prismaClient';
+
+export default async function getOwnedFundraisersQuery() {
+    return prismaClient.fundraiser.findMany({
+        where: { ownerId: 'cltddzx6v0000l3jwegnt4ikb' }
+    });
+}
