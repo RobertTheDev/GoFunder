ccc0956139828b08acc3d01e58fd196bb81bd3dd
RobertTheDev <robertthedev@gmail.com>
2024-03-12 11:12:53 +0000

    ci: deleted files

--- src/app/api/auth/session/route.ts
+++
@@ -1,9 +0,0 @@
-import { cookies } from "next/headers";
-import { getIronSession } from "iron-session";
-import { sessionCookie } from "@/app/api/configs/auth/session";
-
-export async function GET() {
-    const session = await getIronSession(cookies(), sessionCookie);
-
-    return Response.json({ data: session });
-}
