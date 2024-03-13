dabfb57b6b8891e9f54d4462327f34c779ab9575
RobertTheDev <robertthedev@gmail.com>
2024-03-13 06:23:48 +0000

    feat: defined password reset message

---
+++ src/app/api/configs/email/messages/passwordReset/index.ts
@@ -0,0 +1,9 @@
+const passwordResetMessage = {
+    to: "robertthedev@gmail.com",
+    from: "robertthedev@gmail.com",
+    subject: "Password Reset",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+};
+
+export default passwordResetMessage;
