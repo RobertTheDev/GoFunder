7752b4a3a929deef853d6b7faf78a47065f068e7
RobertTheDev <robertthedev@gmail.com>
2024-03-13 06:11:07 +0000

    feat: fetched profile data

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,7 +2,10 @@
 
 // The relevant imports required for the page.
 import { Metadata } from "next";
-import ProfileTemplate from "@/app/modules/profile/templates/ProfileTemplate";
+import { headers } from "next/headers";
+
+// import ProfileTemplate from "@/app/modules/profile/templates/ProfileTemplate";
+// import useSession from "../hooks/session/useSession";
 
 // Metadata defines the seo options for this page.
 export const metadata: Metadata = {
@@ -10,6 +13,12 @@ export const metadata: Metadata = {
 };
 
 // The handler displays and injects profile data into the profile template.
-export default function ProfilePage() {
-    return <ProfileTemplate />;
+export default async function ProfilePage() {
+    const res = await fetch(`http://localhost:3000/api/profile`, {
+        cache: "no-cache",
+        headers: headers(),
+    });
+    const profile = await res.json();
+
+    return <p>{JSON.stringify(profile)}</p>;
 }
