579558a6aca03350a4e393b88f48c0ba49e84656
RobertTheDev <robertthedev@gmail.com>
2024-03-13 23:17:52 +0000

    refactor: moved auth components directory into auth directory

---
+++ src/app/auth/components/SignUpForm/signUp.schema.ts
@@ -0,0 +1,26 @@
+import { z, object, string } from "zod";
+
+// Schema defines the fields required to sign up a user.
+export const signUpSchema = object({
+    email: string({
+        invalid_type_error: "Email must be of type string",
+        required_error: "Email is required",
+    })
+        .email({ message: "Email must be in valid email format" })
+        .min(1, { message: "Email is required" }),
+    name: string({
+        invalid_type_error: "Name must be of type string",
+        required_error: "Name is required",
+    }).min(1, {
+        message: "Name is required",
+    }),
+    password: string({
+        invalid_type_error: "Password must be of type string",
+        required_error: "Password is required",
+    }).min(8, {
+        message: "Password is required",
+    }),
+});
+
+// Infers the schema as a TypeScipt type.
+export type SignUpSchemaType = z.infer<typeof signUpSchema>;
