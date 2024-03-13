a5acfd47b1a45180bb47db88431df4e491c2f99e
RobertTheDev <robertthedev@gmail.com>
2024-03-13 23:08:58 +0000

    refactor: removed auth layout component

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,8 +1,4 @@
-// PURPOSE: This page displays a form for a user to sign in to their account.
-
-// The relevant imports required for the page.
 import SignInForm from "@/app/modules/auth/components/SignInForm";
-import AuthLayout from "@/app/modules/auth/layouts/AuthLayout";
 import { Metadata } from "next";
 
 // Metadata defines the seo options for this page.
@@ -10,12 +6,7 @@ export const metadata: Metadata = {
     title: "Sign In",
 };
 
-// This handler returns a form to sign in a user.
 // This handler wraps the form component with the auth layout.
 export default function SignInPage() {
-    return (
-        <AuthLayout>
-            <SignInForm />
-        </AuthLayout>
-    );
+    return <SignInForm />;
 }
--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,8 +1,4 @@
-// PURPOSE: This page displays a form for a user to sign up to their account.
-
-// The relevant imports required for the page.
 import SignUpForm from "@/app/modules/auth/components/SignUpForm";
-import AuthLayout from "@/app/modules/auth/layouts/AuthLayout";
 import { Metadata } from "next";
 
 // Metadata defines the seo options for this page.
@@ -11,11 +7,6 @@ export const metadata: Metadata = {
 };
 
 // This handler returns a form to sign up a user.
-// This handler wraps the form component with the auth layout.
 export default function SignUpPage() {
-    return (
-        <AuthLayout>
-            <SignUpForm />
-        </AuthLayout>
-    );
+    return <SignUpForm />;
 }
