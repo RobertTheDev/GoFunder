c5461c2fcf65601e3d9d01589235b97495617fcb
RobertTheDev <robertthedev@gmail.com>
2024-03-13 16:48:29 +0000

    refactor: removed confirm password field

--- src/app/api/account-settings/change-password/changePassword.schema.ts
+++ src/app/api/account-settings/change-password/changePassword.schema.ts
@@ -16,15 +16,6 @@ export const changePasswordSchema = object({
         .max(20, {
             message: "Password must be no longer than 20 characters long",
         }),
-    confirmPassword: string({
-        invalid_type_error: "Confirm password must be of type string",
-        required_error: "Confirm password is required",
-    }).min(1, {
-        message: "Confirm password is required",
-    }),
-}).refine((data) => data.newPassword === data.confirmPassword, {
-    message: "Passwords do not match",
-    path: ["confirmPassword"],
 });
 
 // Infers the schema as a TypeScipt type.
