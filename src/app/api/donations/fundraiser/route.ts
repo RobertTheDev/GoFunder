34a48f28bb7418d943cafadb33a2af998026cabb
RobertTheDev <robertthedev@gmail.com>
2024-03-13 13:47:26 +0000

    ci: deleted handler

--- src/app/api/donations/fundraiser/route.ts
+++
@@ -1,8 +0,0 @@
-import prismaClient from "@/app/api/configs/db/prisma/prismaClient";
-
-export async function GET() {
-    return prismaClient.donation.findMany({
-        include: { user: true },
-        where: { fundraiserId: "cltdit65k0004b88hwrmehyjy" },
-    });
-}
