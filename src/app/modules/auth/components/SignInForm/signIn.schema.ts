9be31a09fbc6a7d8d3e78ad4d0114b69f1f573ca
RobertTheDev <robertthedev@gmail.com>
2024-03-13 06:20:39 +0000

    feat: added password field

--- src/app/modules/auth/components/SignInForm/signIn.schema.ts
+++ src/app/modules/auth/components/SignInForm/signIn.schema.ts
@@ -8,6 +8,12 @@ export const signInSchema = object({
     })
         .email({ message: "Email must be in valid email format" })
         .min(1, { message: "Email is required" }),
+    password: string({
+        invalid_type_error: "Password must be of type string",
+        required_error: "Password is required",
+    }).min(1, {
+        message: "Password is required",
+    }),
 });
 
 // Infers the schema as a TypeScipt type.
