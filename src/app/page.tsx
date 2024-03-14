17a91be1badda5ce1aa41bed1a4fafe865988475
RobertTheDev <robertthedev@gmail.com>
2024-03-14 22:42:56 +0000

    refactor: moved home styles and page to app directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
-import HomeTemplate from "@/app/modules/home/template/HomeTemplate";
+import FundraiserSection from "./components/home/FundraiserSection";
+import HomeBanner from "./components/home/HomeBanner";
+import styles from "./page.module.css";
 
 export default function HomePage() {
-    return <HomeTemplate />;
+    return (
+        <div className={styles.pageContainer}>
+            <HomeBanner />
+            <div className={styles.pageSectionsContainer}>
+                <FundraiserSection category="Animals and Pets" />
+                <FundraiserSection category="Disability" />
+                <FundraiserSection category="International Aid" />
+                <FundraiserSection category="Sports" />
+            </div>
+        </div>
+    );
 }
