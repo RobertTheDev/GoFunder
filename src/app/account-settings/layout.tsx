9da03d8cdbaa63116be4f6f55327bb81871d2cf2
RobertTheDev <robertthedev@gmail.com>
2024-03-17 14:32:32 +0000

    refactor: turned into server component

--- src/app/account-settings/layout.tsx
+++ src/app/account-settings/layout.tsx
@@ -1,20 +1,12 @@
-"use client";
-
 import AccountSettingsMenu from "@/app/account-settings/components/AccountSettingsMenu";
 import { ReactNode } from "react";
 import styles from "./layout.module.css";
 
-export default function AccountSettingsLayout({
-    children,
-}: {
-    children: ReactNode;
-}) {
+export default function AccountSettingsLayout(props: { children: ReactNode }) {
     return (
         <div className={styles.pageContainer}>
-            <div>
-                <AccountSettingsMenu />
-            </div>
-            <div>{children}</div>
+            <AccountSettingsMenu />
+            <div>{props.children}</div>
         </div>
     );
 }
