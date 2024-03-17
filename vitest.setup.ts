dcd74469f3afccffc6310196a6ef4b5012aa075f
RobertTheDev <robertthedev@gmail.com>
2024-03-17 02:21:19 +0000

    feat: installed and configured js dom types

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,7 @@
 import { expect, afterEach } from "vitest";
 import * as matchers from "jest-extended";
 import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
 
 expect.extend(matchers);
 
