3bc5e720e22eaa09cc746bfd15e2361e502a66f0
RobertTheDev <robertthedev@gmail.com>
2024-03-17 02:21:43 +0000

    feat: defined alias pathnames

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     test: {
+        alias: { "@/": new URL("./src/", import.meta.url).pathname },
         globals: true,
         environment: "jsdom",
         setupFiles: "./vitest.setup.ts",
