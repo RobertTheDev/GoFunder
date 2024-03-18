3ed622a6c82700d53dd991a427c8e3b6f0bcc4a9
RobertTheDev <robertthedev@gmail.com>
2024-03-18 02:49:24 +0000

    feat: created error page

---
+++ src/app/error.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import ErrorTemplate from "@/app/templates/error";
+import { useEffect } from "react";
+
+export default function Error({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string };
+    reset: () => void;
+}) {
+    useEffect(() => {
+        console.error(error);
+    }, [error]);
+
+    return <ErrorTemplate reset={reset} />;
+}
